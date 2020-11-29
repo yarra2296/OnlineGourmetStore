1eaf3df02ecf7178141f5934cbd06261d4322d54
yarram jaswanth kishore <jaswanth.yarram@agrometrics.com>
2020-11-29 14:09:50 -0600

    added top navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import Home from './components/Home';
+import AddItem from "./components/add-item";
+import PaymentSuccess from "./components/payment-success";
+import UpdateItem from "./components/update-item";
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Router>
+          <div>
+            {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route
+                  exact
+                  path="/"
+                  render={() => {
+                    return (<Redirect to="/" />)
+                  }}
+              />
+              <Route path="/addItem">
+                <AddItem />
+              </Route>
+              <Route path="/updateItem">
+                <UpdateItem />
+              </Route>
+              <Route path="/paymentSuccess">
+                <PaymentSuccess />
+              </Route>
+            </Switch>
+
+          </div>
+        </Router>
+      </div>
   );
 }
 
